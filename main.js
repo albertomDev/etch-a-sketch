dda965363fc24abba1c83ee10d28afcd8627049e
Alberto <medina.v@pm.me>
2023-07-21 12:44:04 +0100

    create grids based on the slider input

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const slider = document.querySelector("#slider")
-const sliderValue = document.querySelector('#slider-value')
+const sliderInput = document.querySelector("#slider");
+const adjustableGrid = document.querySelector(".adjustable-grid");
+const sliderLabel = document.querySelector(".slider-label");
 
+// update grid size and create div
+const gridInputHandler = (event) => {
+  const gridSize = event.currentTarget.value;
+  adjustableGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  adjustableGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  sliderLabel.textContent = `${gridSize}`;
+
+  let totalDiv = "";
+  for (let numDiv = 0; numDiv < gridSize * gridSize; numDiv++) {
+    totalDiv += `<div></div>`;
+  }
+  adjustableGrid.innerHTML = totalDiv;
+};
+
+sliderInput.addEventListener("input", gridInputHandler);
