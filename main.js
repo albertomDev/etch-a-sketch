ecf576fbdb53cab86af4161e1060044fc9cad107
Alberto <medina.v@pm.me>
2023-07-21 15:56:12 +0100

    add background color on click

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const sliderInput = document.querySelector("#slider");
 const adjustableGrid = document.querySelector(".adjustable-grid");
 const sliderLabel = document.querySelector(".slider-label");
+const gridChildren = document.querySelectorAll(".adjustable-grid div");
+const colorPickerInput = document.querySelector("#color-picker");
+let holding = false;
 
 // update grid size and create div
 const gridInputHandler = (event) => {
@@ -16,4 +19,25 @@ const gridInputHandler = (event) => {
   adjustableGrid.innerHTML = totalDiv;
 };
 
+const clickAndHoldHandler = () => {
+  adjustableGrid.addEventListener("mouseover", (event) => {
+    console.log(event.toElement);
+    if (holding && event.currentTarget === adjustableGrid) {
+      console.log("im inside the grid");
+      clickAndHoldHandler(event);
+    }
+  });
+};
+
 sliderInput.addEventListener("input", gridInputHandler);
+
+adjustableGrid.addEventListener("mousedown", (event) => {
+  holding = true;
+  console.log(event.target);
+  event.target.style.backgroundColor = `${colorPickerInput.value}`;
+  clickAndHoldHandler();
+});
+
+document.addEventListener("mouseup", () => {
+  holding = false;
+});
